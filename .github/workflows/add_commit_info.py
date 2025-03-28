21bba55696beb73f963214bc77ec59b4239f3417
FTS427 <FTS427@outlook.com>
2025-03-29 07:50:13 +0800

    U | update build.yml and add_commit_info.py

--- .github/workflows/add_commit_info.py
+++ .github/workflows/add_commit_info.py
@@ -1,14 +1,28 @@
 import sys
-def alter(file,old_str,new_str):
+
+def alter(file, old_str, new_str):
     file_data = ""
+    found = False
     with open(file, "r", encoding="utf-8") as f:
         for line in f:
             if old_str in line:
                 line = line.replace(old_str, new_str)
+                found = True
             file_data += line
-    with open(file,"w",encoding="utf-8") as f:
-        f.write(file_data)
+    if found:
+        with open(file, "w", encoding="utf-8") as f:
+            f.write(file_data)
+    else:
+        print(f"Warning: '{old_str}' not found in {file}.")
+
+if len(sys.argv) < 2:
+    print("Error: Missing commit SHA as an argument.")
+    sys.exit(1)
 
 commit_sha = sys.argv[1]
 
-alter("src/include/info.hpp", "#define PROJECT_VERSION \"1.0.1\"", f"#define PROJECT_VERSION \"1.0.1 {commit_sha[:7]}\"")
+alter(
+    "CMakeLists.txt",
+    'add_definitions(-DPROJECT_VERSION=\"${PROJECT_VERSION}\")',
+    'add_definitions(-DPROJECT_VERSION=\"${PROJECT_VERSION} ' + f'{commit_sha[:7]}\")'
+)
