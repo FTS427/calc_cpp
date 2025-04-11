428a2327e9408b42e4bd4690959f9389177145ce
FTS427 <FTS427@outlook.com>
2025-04-11 19:17:08 +0800

    U | define function as a namespace

--- src/main.cpp
+++ src/main.cpp
@@ -9,19 +9,20 @@
 #include "module/calc.cpp"
 #include "module/function.cpp"
 
+using function::error;
+
 int main(int argc, char *argv[]){
-    Function function;
     Calculate calc(0, ' ', 0);
     
     if (argc > 1){
         if (strcmp(argv[1], "-v") == 0 || strcmp(argv[1], "--version") == 0 ){
-            function.version();
+            function::version();
         }
         else if (strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "--help") == 0 ){
-            function.help();
+            function::help();
         }
         else if (strcmp(argv[1], "-i") == 0 || strcmp(argv[1], "--info") == 0 ){
-            function.info();
+            function::info();
         }
         else if (strcmp(argv[1], "-c") == 0 || strcmp(argv[1], "--calc") == 0 && argc >= 5){
             calc = Calculate(atof(argv[2]), *argv[3], atof(argv[4]));
@@ -31,15 +32,15 @@ int main(int argc, char *argv[]){
                 printf("%s %s%s%s %s %s=%s %lf\n", argv[2], F_YELLOW, argv[3], RESET, argv[4],F_YELLOW ,RESET, calc.get_resault());
             }
             else{
-                function.error();
+                error();
             }
         }
         else{
-            function.error();
+            error();
         }
     }
     else {
-        function.error();
+        error();
     }
     return 0;
 }
--- src/module/function.cpp
+++ src/module/function.cpp
@@ -1,25 +1,27 @@
-class Function{
-    public:
-        void help(){
-            printf("Usage: calc [option]\n");
-            printf("Options:\n");
-            printf(" -v, --version     Show version\n");
-            printf(" -i, --info        Show info\n");
-            printf(" -h, --help        Show help\n");
-            printf(" -c, --calc NUM1 OPTION NUM2      Calculate\n");
-        }
-        void info(){
-            printf("==== %s ====\n", PROJECT_NAME);
-            printf(" - Version: %s build\n", PROJECT_VERSION);
-            printf(" - Repository: %s%s%s\n", UNDERLINE, PROJECT_REPO, RESET);
-            printf(" - License: %s\n", PROJECT_LICENSE);
-            printf(" - Description: %s\n", PROJECT_DESCRIPTION);
-            printf(" - Made by %s\n", PROJECT_AUTHOR);
-        }
-        void version(){
-            printf("%s\n", PROJECT_VERSION);
-        }
-        void error(){
-            printf("%s%s%sInvalid operation!\n%s", ALARM, F_RED, BOLD, RESET);
-        }
-};
+namespace function {
+
+    void help() {
+        printf("Usage: calc [option]\n");
+        printf("Options:\n");
+        printf(" -v, --version     Show version\n");
+        printf(" -i, --info        Show info\n");
+        printf(" -h, --help        Show help\n");
+        printf(" -c, --calc NUM1 OPTION NUM2      Calculate\n");
+    }
+    
+    void info() {
+        printf("==== %s ====\n", PROJECT_NAME);
+        printf(" - Version: %s build\n", PROJECT_VERSION);
+        printf(" - Repository: %s%s%s\n", UNDERLINE, PROJECT_REPO, RESET);
+        printf(" - License: %s\n", PROJECT_LICENSE);
+        printf(" - Description: %s\n", PROJECT_DESCRIPTION);
+        printf(" - Made by %s\n", PROJECT_AUTHOR);
+    }
+    
+    void version() { printf("%s\n", PROJECT_VERSION); }
+    
+    void error() {
+        printf("%s%s%sInvalid operation!\n%s", ALARM, F_RED, BOLD, RESET);
+    }
+
+} // namespace function
